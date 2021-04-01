aa8afd72bf774d3b9a8163a8fce98680ecc491cf
MykalB081 <mykalb081@gmail.com>
2021-04-01 18:44:51 -0400

    Generic Views implemented

--- vote_app/polls/views.py
+++ vote_app/polls/views.py
@@ -7,8 +7,29 @@ from django.urls import reverse
 from django.template import loader
 from django.http import Http404
 
+from django.views import generic
 
-def index(request):
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+'''def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
@@ -31,6 +52,7 @@ def results(request, question_id):
 
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+'''
 
 
 def vote(request, question_id):
